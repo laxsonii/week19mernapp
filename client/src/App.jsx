8c336865d6c7ba584bcb2bd560b44e340f992e2a
NoushinP <noushinazari89@gmail.com>
2024-08-29 21:10:55 -0500

    client folder start

---
+++ client/src/App.jsx
@@ -0,0 +1,12 @@
+import MenuList from './components/MenuList'
+
+function App() {
+
+  return (
+    <>
+      <MenuList />
+    </>
+  )
+}
+
+export default App
---
+++ client/src/components/MenuList.jsx
@@ -0,0 +1,9 @@
+function MenuList() {
+    return(
+        <div>
+            <h1>Welcome to Delicious Site</h1>
+        </div>
+    )
+}
+
+export default MenuList
